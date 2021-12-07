a5f46ced84b90ba245900075a01a6fd15e071a7a
Ayushi <aysharma@ucsd.edu>
2021-12-07 00:55:36 -0800

    refactoring createRecipe and main.js files

--- source/scripts/createRecipe.js
+++ source/scripts/createRecipe.js
@@ -1,279 +1,277 @@
-import * as util from "./API/utilityFunctions.js";
+import * as util from './API/utilityFunctions.js'
 
-let recipeImg = document.getElementById('recipe-img');
-let compressedImg; // store base64 compressed image (string)
+const recipeImg = document.getElementById('recipe-img')
+let compressedImg // store base64 compressed image (string)
 
-let addIng = document.getElementById("add-ing");
-let addStep = document.getElementById("add-step");
-let addNutrition = document.getElementById("add-nutrition");
+const addIng = document.getElementById('add-ing')
+const addStep = document.getElementById('add-step')
+const addNutrition = document.getElementById('add-nutrition')
 
-let createRecipe = document.getElementById("create-btn");
-let cancelBtn = document.getElementById("cancel-btn");
+const createRecipe = document.getElementById('create-btn')
+const cancelBtn = document.getElementById('cancel-btn')
 
-window.addEventListener("DOMContentLoaded", e => {
-    if(window.location.hash){
-        let id = window.location.hash.slice(1);
-        let recipeData = JSON.parse(localStorage.getItem(id));
-        populateRecipeForm(recipeData);
-        createRecipe.innerText = "Update";
-    }
-});
+window.addEventListener('DOMContentLoaded', e => {
+  if (window.location.hash) {
+    const id = window.location.hash.slice(1)
+    const recipeData = JSON.parse(localStorage.getItem(id))
+    populateRecipeForm(recipeData)
+    createRecipe.innerText = 'Update'
+  }
+})
 
 recipeImg.addEventListener('change', (e) => {
-    const input = e.target.files[0];
-    const previewImg = document.getElementById('preview-img');
-    const fileReader = new FileReader();
+  const input = e.target.files[0]
+  const previewImg = document.getElementById('preview-img')
+  const fileReader = new FileReader()
 
-    fileReader.addEventListener('load', (event) => {
-        previewImg.src = event.target.result;
-        previewImg.classList.remove('no-preview');
+  fileReader.addEventListener('load', (event) => {
+    previewImg.src = event.target.result
+    previewImg.classList.remove('no-preview')
 
-        // resizing and compressing input image using <canvas>
-        previewImg.addEventListener('load', (imgData) => {
-            const canvas = document.createElement('canvas');
-            const FIXED_WIDTH = 312 * 2;
-            const FIXED_HEIGHT = 231 * 2;
+    // resizing and compressing input image using <canvas>
+    previewImg.addEventListener('load', (imgData) => {
+      const canvas = document.createElement('canvas')
+      const FIXED_WIDTH = 312 * 2
+      const FIXED_HEIGHT = 231 * 2
 
-            canvas.width = FIXED_WIDTH;
-            canvas.height = FIXED_HEIGHT;
+      canvas.width = FIXED_WIDTH
+      canvas.height = FIXED_HEIGHT
 
-            const ctx = canvas.getContext('2d');
-            ctx.drawImage(imgData.target, 0, 0, canvas.width, canvas.height);
-            const imgEncoded = ctx.canvas.toDataURL(imgData, 'image/jpeg');
-            compressedImg = imgEncoded;
-        })
-    });
+      const ctx = canvas.getContext('2d')
+      ctx.drawImage(imgData.target, 0, 0, canvas.width, canvas.height)
+      const imgEncoded = ctx.canvas.toDataURL(imgData, 'image/jpeg')
+      compressedImg = imgEncoded
+    })
+  })
 
-    if (input) {
-        fileReader.readAsDataURL(input);
-    }
-});
+  if (input) {
+    fileReader.readAsDataURL(input)
+  }
+})
 
-addIng.addEventListener("click", e => {
-    let elementContainer = document.querySelectorAll(".ingredient-container");
-    let elementContainerLength = elementContainer.length;
-    let fieldSet = document.createElement("fieldset");
-    let fieldSetLabel = document.createElement("label");
-    let fieldTextArea = document.createElement("textarea");
-    let deleteBtn = document.createElement("button");
-    
-    deleteBtn.type = "button";
-    deleteBtn.classList.add("remove-btn");
-    let removeIcon = document.createElement("img");
-    removeIcon.src = "./source/image/icons8-delete.svg";
-    removeIcon.style.width = "30px";
-    deleteBtn.appendChild(removeIcon);
-    deleteBtn.addEventListener('click', removeItem);
+addIng.addEventListener('click', e => {
+  const elementContainer = document.querySelectorAll('.ingredient-container')
+  const elementContainerLength = elementContainer.length
+  const fieldSet = document.createElement('fieldset')
+  const fieldSetLabel = document.createElement('label')
+  const fieldTextArea = document.createElement('textarea')
+  const deleteBtn = document.createElement('button')
 
-    fieldSetLabel.innerText = "Ingredient: ";
+  deleteBtn.type = 'button'
+  deleteBtn.classList.add('remove-btn')
+  const removeIcon = document.createElement('img')
+  removeIcon.src = './source/image/icons8-delete.svg'
+  removeIcon.style.width = '30px'
+  deleteBtn.appendChild(removeIcon)
+  deleteBtn.addEventListener('click', removeItem)
 
-    fieldTextArea.name = "ingredients";
+  fieldSetLabel.innerText = 'Ingredient: '
 
-    fieldTextArea.classList.add("ing-name");
-    // fieldTextArea.type = 'text';
-     fieldTextArea.cols = "30";
-     fieldTextArea.rows = "2";
+  fieldTextArea.name = 'ingredients'
 
+  fieldTextArea.classList.add('ing-name')
+  // fieldTextArea.type = 'text';
+  fieldTextArea.cols = '30'
+  fieldTextArea.rows = '2'
 
-    fieldSet.classList.add("ingredient-container");
+  fieldSet.classList.add('ingredient-container')
 
-    fieldSet.appendChild(fieldSetLabel);
-    fieldSet.appendChild(fieldTextArea);
-    fieldSet.appendChild(deleteBtn);
+  fieldSet.appendChild(fieldSetLabel)
+  fieldSet.appendChild(fieldTextArea)
+  fieldSet.appendChild(deleteBtn)
 
-    elementContainer[elementContainerLength-1].after(fieldSet);
-});
+  elementContainer[elementContainerLength - 1].after(fieldSet)
+})
 
-addStep.addEventListener("click", e => {
-    let elementContainer = document.querySelectorAll(".steps-container");
-    let elementContainerLength = elementContainer.length;
-    let fieldSet = document.createElement("fieldset");
-    let fieldSetLabel = document.createElement("label");
-    let fieldTextArea = document.createElement("textarea");
-    let deleteBtn = document.createElement("button");
-    
-    deleteBtn.type = "button";
-    deleteBtn.classList.add("remove-btn");
-    let removeIcon = document.createElement("img");
-    removeIcon.src = "./source/image/icons8-delete.svg";
-    removeIcon.style.width = "30px";
-    deleteBtn.appendChild(removeIcon);
-    deleteBtn.addEventListener('click', removeItem);
+addStep.addEventListener('click', e => {
+  const elementContainer = document.querySelectorAll('.steps-container')
+  const elementContainerLength = elementContainer.length
+  const fieldSet = document.createElement('fieldset')
+  const fieldSetLabel = document.createElement('label')
+  const fieldTextArea = document.createElement('textarea')
+  const deleteBtn = document.createElement('button')
 
-    fieldSetLabel.innerText = "Step: ";
-    
-    fieldTextArea.name = "steps";
+  deleteBtn.type = 'button'
+  deleteBtn.classList.add('remove-btn')
+  const removeIcon = document.createElement('img')
+  removeIcon.src = './source/image/icons8-delete.svg'
+  removeIcon.style.width = '30px'
+  deleteBtn.appendChild(removeIcon)
+  deleteBtn.addEventListener('click', removeItem)
 
-    fieldTextArea.classList.add("stepName");
-    // fieldTextArea.type = 'text';
-    fieldTextArea.cols = "50";
-    fieldTextArea.rows = "5";
+  fieldSetLabel.innerText = 'Step: '
 
-    fieldTextArea.classList.add("step-name");
-    //fieldTextArea.type = 'text';
-    fieldTextArea.cols = "50";
-    fieldTextArea.rows = "5";
+  fieldTextArea.name = 'steps'
 
-    
-    fieldSet.classList.add("steps-container");
-    fieldSet.appendChild(fieldSetLabel);
-    fieldSet.appendChild(fieldTextArea);
-    fieldSet.appendChild(deleteBtn);
-    
-    elementContainer[elementContainerLength-1].after(fieldSet); 
-});
+  fieldTextArea.classList.add('stepName')
+  // fieldTextArea.type = 'text';
+  fieldTextArea.cols = '50'
+  fieldTextArea.rows = '5'
 
-addNutrition.addEventListener("click", e => {
-    let elementContainer = document.querySelectorAll(".nutrition-container");
-    let elementContainerLength = elementContainer.length;
-    let fieldSet = document.createElement("fieldset");
-    let fieldSetLabel = document.createElement("label");
-    let fieldTextArea = document.createElement("textarea");
-    let deleteBtn = document.createElement("button");
-    
-    deleteBtn.type = "button";
-    deleteBtn.classList.add("remove-btn");
-    let removeIcon = document.createElement("img");
-    removeIcon.src = "./source/image/icons8-delete.svg";
-    removeIcon.style.width = "30px";
-    deleteBtn.appendChild(removeIcon);
-    deleteBtn.addEventListener('click', removeItem);
+  fieldTextArea.classList.add('step-name')
+  // fieldTextArea.type = 'text';
+  fieldTextArea.cols = '50'
+  fieldTextArea.rows = '5'
 
-    fieldSetLabel.innerText = "Nutrition: ";
-    
-    fieldTextArea.name = "nutrition";
+  fieldSet.classList.add('steps-container')
+  fieldSet.appendChild(fieldSetLabel)
+  fieldSet.appendChild(fieldTextArea)
+  fieldSet.appendChild(deleteBtn)
 
-    fieldTextArea.classList.add("nutr-name");
-    //fieldTextArea.type = 'text';
-     fieldTextArea.cols = "30";
-     fieldTextArea.rows = "2";
-    
-    fieldSet.classList.add("nutrition-container");
-    fieldSet.appendChild(fieldSetLabel);
-    fieldSet.appendChild(fieldTextArea);
-    fieldSet.appendChild(deleteBtn);
-    
-    elementContainer[elementContainerLength-1].after(fieldSet);
-});
+  elementContainer[elementContainerLength - 1].after(fieldSet)
+})
 
-function removeItem(e){
-    let i = e.path[0].tagName === "IMG" ? 0 : 1;
-    let parentContainer = e.path[3-i];
-    let fieldSet = e.path[2-i];
-    parentContainer.removeChild(fieldSet);
+addNutrition.addEventListener('click', e => {
+  const elementContainer = document.querySelectorAll('.nutrition-container')
+  const elementContainerLength = elementContainer.length
+  const fieldSet = document.createElement('fieldset')
+  const fieldSetLabel = document.createElement('label')
+  const fieldTextArea = document.createElement('textarea')
+  const deleteBtn = document.createElement('button')
+
+  deleteBtn.type = 'button'
+  deleteBtn.classList.add('remove-btn')
+  const removeIcon = document.createElement('img')
+  removeIcon.src = './source/image/icons8-delete.svg'
+  removeIcon.style.width = '30px'
+  deleteBtn.appendChild(removeIcon)
+  deleteBtn.addEventListener('click', removeItem)
+
+  fieldSetLabel.innerText = 'Nutrition: '
+
+  fieldTextArea.name = 'nutrition'
+
+  fieldTextArea.classList.add('nutr-name')
+  // fieldTextArea.type = 'text';
+  fieldTextArea.cols = '30'
+  fieldTextArea.rows = '2'
+
+  fieldSet.classList.add('nutrition-container')
+  fieldSet.appendChild(fieldSetLabel)
+  fieldSet.appendChild(fieldTextArea)
+  fieldSet.appendChild(deleteBtn)
+
+  elementContainer[elementContainerLength - 1].after(fieldSet)
+})
+
+function removeItem (e) {
+  const i = e.path[0].tagName === 'IMG' ? 0 : 1
+  const parentContainer = e.path[3 - i]
+  const fieldSet = e.path[2 - i]
+  parentContainer.removeChild(fieldSet)
 }
 
-createRecipe.addEventListener("click", e => {
-    e.preventDefault();
-    let formRes = {
-        "id": "",
-        "image": "",
-        "favorite": true,
-        "readyInMinutes": 0,
-        "servingSize": 0,
-        "title": "",
-        "summary": "",
-        "ingredients": [],
-        "ingredientSearch": "",
-        "steps": [],
-        "nutrition": []
-    };
-    let formData = document.getElementById("recipe-form");
-    let formObj = new FormData(formData);
-    let formKeys = Array.from(formObj.keys());
-    let hash = window.location.hash;
+createRecipe.addEventListener('click', e => {
+  e.preventDefault()
+  const formRes = {
+    id: '',
+    image: '',
+    favorite: true,
+    readyInMinutes: 0,
+    servingSize: 0,
+    title: '',
+    summary: '',
+    ingredients: [],
+    ingredientSearch: '',
+    steps: [],
+    nutrition: []
+  }
+  const formData = document.getElementById('recipe-form')
+  const formObj = new FormData(formData)
+  const formKeys = Array.from(formObj.keys())
+  const hash = window.location.hash
 
-    if(hash){
-        let currRecipe = JSON.parse(localStorage.getItem(hash.slice(1)));
-        if(hash.slice(1,4) === "ucr"){
-            let title = formObj.get("title");
-            if(title !== currRecipe.title){
-                localStorage.removeItem(hash.slice(1));
-            }
-            formRes["id"] = "ucr_" + title.replaceAll(" ", "");
-        }else{
-            formRes["id"] = currRecipe.id;
-            formRes["image"] = currRecipe.image;
-            formRes["favorite"] = false;
-        }
-    }else{
-        let title = formObj.get("title");
-        formRes["id"] = "ucr_" + title.replaceAll(" ", "");
-        util.addFavoriteRecipe(formRes["id"]);
+  if (hash) {
+    const currRecipe = JSON.parse(localStorage.getItem(hash.slice(1)))
+    if (hash.slice(1, 4) === 'ucr') {
+      const title = formObj.get('title')
+      if (title !== currRecipe.title) {
+        localStorage.removeItem(hash.slice(1))
+      }
+      formRes.id = 'ucr_' + title.replaceAll(' ', '')
+    } else {
+      formRes.id = currRecipe.id
+      formRes.image = currRecipe.image
+      formRes.favorite = false
     }
+  } else {
+    const title = formObj.get('title')
+    formRes.id = 'ucr_' + title.replaceAll(' ', '')
+    util.addFavoriteRecipe(formRes.id)
+  }
 
-    formKeys.forEach(key => {
-        let res = formObj.getAll(key);
+  formKeys.forEach(key => {
+    const res = formObj.getAll(key)
 
-        if(key == "steps" || key === "nutrition" || key == "ingredients"){
-            formRes[key] = formObj.getAll(key);
-        }else if(key === "recipeDesc"){
-            formRes['summary'] = res[0];
-        }else if (key === 'image') {
-            const defaultImg = "../../admin/branding/logo3_231x231.jpg";
-            formRes[key] = (compressedImg ? compressedImg : defaultImg);
-        }else{
-            formRes[key] = res[0];
-        }
-    });
+    if (key === 'steps' || key === 'nutrition' || key === 'ingredients') {
+      formRes[key] = formObj.getAll(key)
+    } else if (key === 'recipeDesc') {
+      formRes.summary = res[0]
+    } else if (key === 'image') {
+      const defaultImg = '../../admin/branding/logo3_231x231.jpg'
+      formRes[key] = (compressedImg || defaultImg)
+    } else {
+      formRes[key] = res[0]
+    }
+  })
 
-    formRes["ingredients"].forEach(ing => {
-        formRes["ingredientSearch"] += ing;
-    });
-    
-    util.setLocalStorageItem(formRes["id"], formRes);
-    window.location.href = "/index.html";
-});
+  formRes.ingredients.forEach(ing => {
+    formRes.ingredientSearch += ing
+  })
+
+  util.setLocalStorageItem(formRes.id, formRes)
+  window.location.href = '/index.html'
+})
 
 cancelBtn.addEventListener('click', e => {
-    window.location.href = "/index.html";
-});
+  window.location.href = '/index.html'
+})
 
-function populateRecipeForm(recipeData){
-    document.getElementById("recipe-name").value = recipeData.title;
-    document.getElementById("recipe-time").value = recipeData.readyInMinutes;
-    document.getElementById("recipe-desc").value = recipeData.summary;
-    document.getElementById("recipe-serve").value = recipeData.servingSize;
+function populateRecipeForm (recipeData) {
+  document.getElementById('recipe-name').value = recipeData.title
+  document.getElementById('recipe-time').value = recipeData.readyInMinutes
+  document.getElementById('recipe-desc').value = recipeData.summary
+  document.getElementById('recipe-serve').value = recipeData.servingSize
 
-    // Image fill-in
-    let previewImg = document.getElementById('preview-img');
-    const defaultImg = "../../admin/branding/logo3_231x231.jpg";
+  // Image fill-in
+  const previewImg = document.getElementById('preview-img')
+  const defaultImg = '../../admin/branding/logo3_231x231.jpg'
 
-    if (recipeData.image === defaultImg) {
-        compressedImg = defaultImg;
-    } else {
-        previewImg.src = recipeData.image; 
-        compressedImg = recipeData.image;
-        previewImg.classList.remove('no-preview');
-    }
-    
-    // Ingredients fill-in
-    let numIng = recipeData.ingredients.length;
-    for(let i = 0; i < numIng-1; i++){
-        addIng.click();
-    }
-    let ingInputs = document.getElementsByClassName("ing-name");
-    for(let i = 0; i < numIng; i++){
-        ingInputs[i].value = recipeData.ingredients[i];
-    }
+  if (recipeData.image === defaultImg) {
+    compressedImg = defaultImg
+  } else {
+    previewImg.src = recipeData.image
+    compressedImg = recipeData.image
+    previewImg.classList.remove('no-preview')
+  }
 
-    // Steps fill-in
-    let numSteps = recipeData.steps.length;
-    for(let i = 0; i < numSteps-1; i++){
-        addStep.click();
-    }
-    let stepInputs = document.getElementsByClassName("step-name");
-    for(let i = 0; i < numSteps; i++){
-        stepInputs[i].value = recipeData.steps[i];
-    }
+  // Ingredients fill-in
+  const numIng = recipeData.ingredients.length
+  for (let i = 0; i < numIng - 1; i++) {
+    addIng.click()
+  }
+  const ingInputs = document.getElementsByClassName('ing-name')
+  for (let i = 0; i < numIng; i++) {
+    ingInputs[i].value = recipeData.ingredients[i]
+  }
 
-    // Nutrition fill-in
-    let numNutrients = recipeData.nutrition.length;
-    for(let i = 0; i < numNutrients-1; i++){
-        addNutrition.click();
-    }
-    let nutritionInputs = document.getElementsByClassName("nutr-name");
-    for(let i = 0; i < numNutrients; i++){
-        nutritionInputs[i].value = recipeData.nutrition[i];
-    }
+  // Steps fill-in
+  const numSteps = recipeData.steps.length
+  for (let i = 0; i < numSteps - 1; i++) {
+    addStep.click()
+  }
+  const stepInputs = document.getElementsByClassName('step-name')
+  for (let i = 0; i < numSteps; i++) {
+    stepInputs[i].value = recipeData.steps[i]
+  }
+
+  // Nutrition fill-in
+  const numNutrients = recipeData.nutrition.length
+  for (let i = 0; i < numNutrients - 1; i++) {
+    addNutrition.click()
+  }
+  const nutritionInputs = document.getElementsByClassName('nutr-name')
+  for (let i = 0; i < numNutrients; i++) {
+    nutritionInputs[i].value = recipeData.nutrition[i]
+  }
 }
--- source/scripts/main.js
+++ source/scripts/main.js
@@ -2,114 +2,112 @@
  * Homepage JS file
  */
 
-import * as util from "./API/utilityFunctions.js";
+import * as util from './API/utilityFunctions.js'
 
-window.addEventListener("DOMContentLoaded", init);
+window.addEventListener('DOMContentLoaded', init)
 
-document.getElementById("create-recipe").addEventListener('click', e => {
-    window.location.href = "/createRecipe.html";
-});
+document.getElementById('create-recipe').addEventListener('click', e => {
+  window.location.href = '/createRecipe.html'
+})
 
 /**
  * Function that runs when the page loads
  */
-function init(){
-    getUserPrefs();
-  
-    // display the 5 recipes and add search btn listener only in homepage
-    if (window.location.pathname === '/index.html' || window.location.pathname === '/' || window.location.pathname === '') {
-        createRecipeCards(util.NUMBER_OF_RECIPES_TO_DISPLAY)
-        let searchBtn = document.getElementById("search");
-        searchBtn.addEventListener("click", e => {
-            e.preventDefault();
-            let searchQuery = document.getElementById("search-query").value;
-            util.searchLocalRecipes(searchQuery).then(searchResults => {
-                let resIds = [];
-                searchResults.forEach(recipe => {
-                    resIds.push(recipe["id"]);
+function init () {
+  getUserPrefs()
 
-                });
-                let searchObj = {
-                    "data": resIds,
-                    "query": searchQuery,
-                    "matchedCount": searchResults.length
-                }
-                localStorage.setItem("latestSearch", JSON.stringify(searchObj));
-                window.location.href = "/searchpage.html";
-            });
-        });
-    }
+  // display the 5 recipes and add search btn listener only in homepage
+  if (window.location.pathname === '/index.html' || window.location.pathname === '/' || window.location.pathname === '') {
+    createRecipeCards(util.NUMBER_OF_RECIPES_TO_DISPLAY)
+    const searchBtn = document.getElementById('search')
+    searchBtn.addEventListener('click', e => {
+      e.preventDefault()
+      const searchQuery = document.getElementById('search-query').value
+      util.searchLocalRecipes(searchQuery).then(searchResults => {
+        const resIds = []
+        searchResults.forEach(recipe => {
+          resIds.push(recipe.id)
+        })
+        const searchObj = {
+          data: resIds,
+          query: searchQuery,
+          matchedCount: searchResults.length
+        }
+        localStorage.setItem('latestSearch', JSON.stringify(searchObj))
+        window.location.href = '/searchpage.html'
+      })
+    })
+  }
 
-    // let searchBtn = document.getElementById("search");
-    // searchBtn.addEventListener("click", e => {
-    //     e.preventDefault();
-    //     let searchQuery = document.getElementById("search-query").value;
-    //     util.searchLocalRecipes(searchQuery).then(arr => {
-    //         let res = [];
-    //         arr.forEach(recipe => {
-    //             res.push(recipe["id"]);
-    //         });
-    //         console.log(res);
-    //         let searchObj = {
-    //             "data": res,
-    //             "query": searchQuery,
-    //             "matchedCount": arr.length
-    //         }
-    //         localStorage.setItem("latestSearch", JSON.stringify(searchObj));
-    //         window.location.href = "/source/searchpage.html";
-    //     });
-    // });
-
+  // let searchBtn = document.getElementById("search");
+  // searchBtn.addEventListener("click", e => {
+  //     e.preventDefault();
+  //     let searchQuery = document.getElementById("search-query").value;
+  //     util.searchLocalRecipes(searchQuery).then(arr => {
+  //         let res = [];
+  //         arr.forEach(recipe => {
+  //             res.push(recipe["id"]);
+  //         });
+  //         console.log(res);
+  //         let searchObj = {
+  //             "data": res,
+  //             "query": searchQuery,
+  //             "matchedCount": arr.length
+  //         }
+  //         localStorage.setItem("latestSearch", JSON.stringify(searchObj));
+  //         window.location.href = "/source/searchpage.html";
+  //     });
+  // });
 }
 
 /**
  * Create recipe cards for an arbitrary amount of recipes
- * 
+ *
  * @param {number} N The number of recipes to display
  * @param {boolean} search If you're creating recipe cards from a search query
- * 
+ *
  */
-export function createRecipeCards(N){ 
-    let recipes = util.getNRandomRecipes(N);
-    // Get the recipe cards' section element
-    let recipeCardsSection = document.getElementById("recipe-cards");
+export function createRecipeCards (N) {
+  const recipes = util.getNRandomRecipes(N)
+  // Get the recipe cards' section element
+  const recipeCardsSection = document.getElementById('recipe-cards')
 
-    recipes.forEach(recipe => {
-        const recipeCard = document.createElement("recipe-card");
-        recipeCard.data = recipe;
-        let key = recipe["id"];
-        bindRC(recipeCard, key);
-        recipeCardsSection.appendChild(recipeCard);
-    });
+  recipes.forEach(recipe => {
+    const recipeCard = document.createElement('recipe-card')
+    recipeCard.data = recipe
+    const key = recipe.id
+    bindRC(recipeCard, key)
+    recipeCardsSection.appendChild(recipeCard)
+  })
 }
 
 /**
  * Binds a recipe card to its router page function via the 'click' event
- * 
+ *
  * @param {HTMLElement} recipeCard The element to which we bind an event listener
  * @param {string} key The id of the recipe card
  */
-function bindRC(recipeCard, key){
-    recipeCard.addEventListener("click", e => {
-        window.location.href = "/recipePage.html#"+key;
-    });
+function bindRC (recipeCard, key) {
+  recipeCard.addEventListener('click', e => {
+    window.location.href = '/recipePage.html#' + key
+  })
 }
 
-function getUserPrefs(){
-    if(util.getLocalStorageRecipes().length == 0){
-        util.updateUserData('offset', 0)
-        let intols = prompt("Enter your intolerances (ingredients not to include)\n\nAvailable: dairy, gluten, shellfish, seafood, wheat, eggs, peanut, soy, grain, sesame, tree nut, sulfite");
-        if(intols || intols === ""){
-            util.setIntolerances(intols);
-        }
-
-        let maxTime = prompt("Enter the maximum amount of time you'd want to spend making a recipe (in minutes)");
-        if(maxTime || !isNaN(Number(maxTime))){
-            util.setMaxTime(maxTime);
-        }
-
-        util.populateRecipes(util.DEFAULT_RECIPE_NUMBER).then(() => {
-            createRecipeCards(util.NUMBER_OF_RECIPES_TO_DISPLAY);
-        });
+function getUserPrefs () {
+  if (util.getLocalStorageRecipes().length === 0) {
+    util.updateUserData('offset', 0)
+    const intols = prompt('Enter your intolerances (ingredients not to include)\n\nAvailable: dairy, gluten, shellfish, seafood, wheat, eggs, peanut, soy, grain, sesame, tree nut, sulfite')
+    if (intols || intols === '') {
+      util.setIntolerances(intols)
     }
+
+    const maxTime = prompt("Enter the maximum amount of time you'd want to spend making a recipe (in minutes)")
+    if (maxTime || !isNaN(Number(maxTime))) {
+      util.setMaxTime(maxTime)
+    }
+
+    util.populateRecipes(util.DEFAULT_RECIPE_NUMBER).then(() => {
+      createRecipeCards(util.NUMBER_OF_RECIPES_TO_DISPLAY)
+    })
+  }
 }
