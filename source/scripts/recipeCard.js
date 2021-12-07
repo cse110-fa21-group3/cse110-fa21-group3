e25047261a68f26801a3f28c127e90677f481070
Ayushi <aysharma@ucsd.edu>
2021-12-07 00:45:40 -0800

    Refactored recipeCard.js

--- source/scripts/recipeCard.js
+++ source/scripts/recipeCard.js
@@ -3,19 +3,19 @@
 /**
  * Creates Recipes Cards for the DOM and has styling for it as well
  */
-class RecipeCard extends HTMLElement{
-    constructor(){
-        super();
-        this.attachShadow({ mode: "open" });
-    }
+class RecipeCard extends HTMLElement {
+  constructor () {
+    super()
+    this.attachShadow({ mode: 'open' })
+  }
 
-    set data(data){
-        if(!data) return;
-        this.json = data;
+  set data (data) {
+    if (!data) return
+    this.json = data
 
-        // Recipe card css
-        const style = document.createElement('style');
-        style.innerHTML = `
+    // Recipe card css
+    const style = document.createElement('style')
+    style.innerHTML = `
         * {
             margin: 0;
             padding: 0;
@@ -53,23 +53,23 @@ class RecipeCard extends HTMLElement{
             margin-top: 1rem;
             line-height: 1.5rem;
         }
-        `;
+        `
 
-        const recipeCard = document.createElement("article");
-        const recipeImage = document.createElement("img");
-        const recipeTitle = document.createElement("p");
-        
-        recipeImage.setAttribute("src", data["image"]);
-        recipeTitle.innerText = data["title"];
-        
-        recipeCard.appendChild(recipeImage);
-        recipeCard.appendChild(recipeTitle);
-        this.shadowRoot.append(style, recipeCard);
-    }
+    const recipeCard = document.createElement('article')
+    const recipeImage = document.createElement('img')
+    const recipeTitle = document.createElement('p')
 
-    get data(){
-        return this.json;
-    }
+    recipeImage.setAttribute('src', data.image)
+    recipeTitle.innerText = data.title
+
+    recipeCard.appendChild(recipeImage)
+    recipeCard.appendChild(recipeTitle)
+    this.shadowRoot.append(style, recipeCard)
+  }
+
+  get data () {
+    return this.json
+  }
 }
 
-customElements.define("recipe-card", RecipeCard);
+customElements.define('recipe-card', RecipeCard)
