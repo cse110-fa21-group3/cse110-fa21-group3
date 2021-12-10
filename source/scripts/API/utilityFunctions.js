50c74b935ac71e6de63615df787f52d43c14a8f7
Ayushi Sharma <56704777+ayushiatsharma@users.noreply.github.com>
2021-12-09 22:04:38 -0800

    Update utilityFunctions.js

--- source/scripts/API/utilityFunctions.js
+++ source/scripts/API/utilityFunctions.js
@@ -2,7 +2,6 @@ import * as localStorageHandler from './localStorageHandler.js'
 import API_KEY from './api.js'
 
 // API Key and endpoint
-// const API_KEY = '6c38415312msh8fd80bab0f17271p1dcefajsn83892f0c646f'
 const API_ENDPOINT = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
 
 // These are typically the options we need to perform a request
