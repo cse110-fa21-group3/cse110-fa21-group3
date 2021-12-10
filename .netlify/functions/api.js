ea6111013bd74658add24805a1c19a00ff66e981
Ayushi Sharma <56704777+ayushiatsharma@users.noreply.github.com>
2021-12-09 21:41:07 -0800

    Delete .netlify/functions directory

--- .netlify/functions/api.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-    console.log("testing1")
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-        api: process.env.API_KEY
-        }),
-    }
-}
